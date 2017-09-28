b6d13a542acba582a8adb46b966f10bf555ce81a
James Cox <j-cox03@hotmail.com>
2017-09-28 11:50:53 +0100

    moved server code into folder server_src

---
+++ app/server_src/db/seeds.js
@@ -0,0 +1,19 @@
+use pages;
+db.dropDatabase();
+
+db.pages.insert(
+  [
+    {
+      title: "example page 1",
+      summary: "example summary 1",
+      content: [
+        {
+          title: "content title example 1",
+          type: "text",
+          value: "example text value 1",
+        },
+      ],
+    }
+  ]
+);
+db.pages.find();
