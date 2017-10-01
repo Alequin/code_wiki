60ed1e3e54abbb8657af4fd11fc3272fa4bbf5c6
James Cox <j-cox03@hotmail.com>
2017-10-01 11:12:19 +0100

    added dev override to request auth

--- app/server_src/services/RequestAuth.js
+++ app/server_src/services/RequestAuth.js
@@ -1,6 +1,11 @@
 const SERVER = require("../ServerVariables.js");
 
-const requestAuth = function (req, res, next) {
+function requestAuth(req, res, next) {
+  if(SERVER.DISABLE_AUTH) next();
+  else authKeyCheck(req, res, next);
+}
+
+function authKeyCheck(req, res, next){
   const authKey = req.get(SERVER.AUTH_TITLE);
   if(authKey === SERVER.AUTH_KEY) {
     next();
