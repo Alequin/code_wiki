2def45cd54fe0af390c3ddff53d3cc8e8b8c4bc5
James Cox <j-cox03@hotmail.com>
2017-10-01 11:01:15 +0100

    Included PageController

---
+++ app/controllers/PageController.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pagesRouter = new express.Router();
+const requestAuth = require("./../server_src/services/RequestAuth");
+const accessDB = require("./../server_src/db/MongoConnection.js");
+
+const onError = (error) => {console.log(error.stack)}
+
+pagesRouter.get('/', function(req, res, next){
+  accessDB((db) => {
+    db.collection("pages").find({}).toArray(function(err, result) {
+      res.json(result);
+      db.close();
+    });
+  });
+});
+
+module.exports = pagesRouter;
