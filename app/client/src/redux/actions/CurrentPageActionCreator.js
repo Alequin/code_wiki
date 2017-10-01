e964ed8c5d99068ba8a6a9c9a2207fd1d482f425
James Cox <j-cox03@hotmail.com>
2017-10-01 11:29:29 +0100

    changed current page actions to send title as json

--- app/client/src/redux/actions/CurrentPageActionCreator.js
+++ app/client/src/redux/actions/CurrentPageActionCreator.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export function getCurrentPageByTitle(title) {
 	return {
 		type: 'CURRENT_PAGE',
-		payload: axios.get(`http://localhost:3000/db/wiki/page/${title}`)
+		payload: axios.get(`http://localhost:3000/db/wiki/page`, title)
 	}
 }
