a81e3067d7a9185ba95d76b33df0eb83ec532c19
James Cox <j-cox03@hotmail.com>
2017-10-01 21:15:30 +0100

    Made divider component

---
+++ app/client/src/components/Divider.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import css from "./Divider.scss"
+
+class TextContent extends Component {
+
+  render() {
+
+    return (
+      <div className="divider"></div>
+    )
+  }
+}
+
+export default TextContent;
