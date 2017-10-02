3f67b6baec1acea8341d3f41276d3e1cde8ef281
James Cox <j-cox03@hotmail.com>
2017-10-02 08:41:40 +0100

    added search bar to nav.jsx

--- app/client/src/containers/Nav.jsx
+++ app/client/src/containers/Nav.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom"
 
+import css from "./Nav.scss"
+
 class Index extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Nav</h1>
+      <div className="nav">
+        <h1>Wiki</h1>
+        <label> Search By Page Title
+          <div className="search-container">
+            <input type="search"></input>
+            <button>Search</button>
+          </div>
+        </label>
       </div>
     )
   }
