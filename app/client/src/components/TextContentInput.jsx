e65fd413211c66c6c825c7515eadd0c128a50ded
James Cox <j-cox03@hotmail.com>
2017-10-05 11:56:02 +0100

    Add styles to TextContentInput

--- app/client/src/components/TextContentInput.jsx
+++ app/client/src/components/TextContentInput.jsx
@@ -4,13 +4,18 @@ import css from "./TextContentInput.scss"
 
 class TextContentInput extends Component {
 
+  constructor(){
+    super();
+  }
+
   render() {
     return (
       <div className="input-text-content">
         <input className="tile" type="text" placeholder={this.props.title}/>
-        <div>
-          <textarea className="tile" type="textarea" placeholder={this.props.content}/>
-        </div>
+        <textarea className="tile" type="textarea" placeholder={this.props.content}/>
+        <section className="buttons">
+          <button className="tile">Alter Width</button>
+        </section>
       </div>
     )
   }
