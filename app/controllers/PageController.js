ea0773345e2067dd5c0b1810f19036ed95e2c1b7
James Cox <j-cox03@hotmail.com>
2017-10-07 17:08:23 +0100

    made route for posting new pages

--- app/controllers/PageController.js
+++ app/controllers/PageController.js
@@ -5,6 +5,25 @@ const accessDB = require("./../server_src/db/MongoConnection.js");
 
 const onError = (error) => {console.log(error.stack)}
 
+pagesRouter.post('/', requestAuth, function(req, res, next){
+
+  const page = req.body;
+
+  accessDB((db) => {
+    db.collection("pages").insertOne(page, function(err, r) {
+      if(err){
+        onError();
+        res.json({inserted: false});
+      }
+      else{
+        res.json({inserted: true});
+      }
+      db.close();
+    });
+  });
+
+});
+
 pagesRouter.get('/title/:title', requestAuth, function(req, res, next){
 
   let query = req.params;
