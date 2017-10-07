f8af741d9eb30a75ae9db2ba8cf00b6fb0999618
James Cox <j-cox03@hotmail.com>
2017-10-07 11:44:44 +0100

    made the textarea for summary input have a modifiable width

--- app/client/src/components/SummaryContentInput.jsx
+++ app/client/src/components/SummaryContentInput.jsx
@@ -9,21 +9,51 @@ class SummaryContentInput extends Component {
   constructor(props){
     super(props);
     this.onEditSummary = this.onEditSummary.bind(this)
+    this.onClickAlterWidth = this.onClickAlterWidth.bind(this)
+    this.state = {
+      width: "40%"
+    }
   }
 
   onEditSummary(event){
     this.props.editPageSummary(event.target.value);
   }
 
+  onClickAlterWidth(){
+    let width
+    switch(this.state.width){
+      case "20%":
+        width = "40%"
+        break;
+      case "40%":
+        width = "60%"
+        break;
+      case "60%":
+        width = "80%"
+        break;
+      case "80%":
+        width = "100%"
+        break;
+      case "100%":
+        width = "20%"
+        break;
+    }
+    this.setState({
+      width: width
+    })
+  }
+
   render() {
+    console.log(this.state.textAreaStyles);
     return (
       <div className="content-container">
         <h3>Summary</h3>
         <textarea className="tile" type="textarea" placeholder={this.props.content}
           onChange={this.onEditSummary} value={this.props.summary}
+          style={this.state}
         />
         <section className="content-buttons">
-          <button className="tile hover-tile">Alter Width</button>
+          <button className="tile hover-tile" onClick={this.onClickAlterWidth}>Alter Width</button>
         </section>
       </div>
     )
