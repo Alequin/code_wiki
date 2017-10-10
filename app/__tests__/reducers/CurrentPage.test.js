6e5f84177197e1e8ff03e6de7508a5e101df99d0
James Cox <j-cox03@hotmail.com>
2017-10-10 14:35:05 +0100

    CurrentPage reducer: made tests for DELETE_CURRENT_PAGE actions

--- app/__tests__/reducers/CurrentPage.test.js
+++ app/__tests__/reducers/CurrentPage.test.js
@@ -50,4 +50,49 @@ describe('CurrentPage Reducer', () => {
 		defaultPage.page = payload.data
 		expect(result).toMatchObject(defaultPage)
 	})
+
+	it('DELETE_CURRENT_PAGE_PENDING: should set property fetching to true', () => {
+		const action = {
+			type: "DELETE_CURRENT_PAGE_PENDING"
+		}
+		const result = currentPageReducer(defaultPage, action);
+		defaultPage.fetching = true;
+		expect(result).toMatchObject(defaultPage);
+	})
+
+	it(`DELETE_CURRENT_PAGE_REJECTED: should set fetching to false and add a property error
+		 	with error set to the payload given`, () => {
+		const errorPayload = {error: "returned object"}
+		const action = {
+			type: "DELETE_CURRENT_PAGE_REJECTED",
+			payload: errorPayload
+		}
+
+		const result = currentPageReducer(defaultPage, action)
+		defaultPage.fetching = true
+		defaultPage.error = {error: "returned object"}
+		expect(result).toMatchObject(defaultPage)
+	})
+
+	it(`DELETE_CURRENT_PAGE_FULFILLED: should set fetching and fetched to true and set
+			property page to payload.data`, () => {
+		const payload = {data: new Page("", "")}
+		const action = {
+			type: "DELETE_CURRENT_PAGE_FULFILLED",
+			payload: payload
+		}
+
+		const result = currentPageReducer(defaultPage, action)
+		defaultPage.fetching = true
+		defaultPage.fetched = true
+		defaultPage.page = payload.data
+
+		expect(result.fetching).toBe(defaultPage.fetching)
+		expect(result.fetched).toBe(defaultPage.fetched)
+
+		expect(result.page.title).toBe(defaultPage.page.title)
+		expect(result.page.summary).toBe(defaultPage.page.summary)
+		expect(result.page.content.length).toBe(defaultPage.page.content.length)
+		expect(result.page.tags.length).toBe(defaultPage.page.tags.length)
+	})
 })
